e1907ed3a1963dabea0fd2434592d88cc7a8b94d
Bakhman <begaev7@gmail.com>
2021-01-11 12:42:24 +0300

    Fixed typo in function documentation

--- validations.py
+++ validations.py
@@ -2,7 +2,7 @@
 
 
 def validate_user(username, minlen):
-  """Checks if the recieved username matches the required conditions."""
+  """Checks if the received username matches the required conditions."""
   if type(username) != str:
     raice TypeError("username must be a string")
   if minlen < 1:
